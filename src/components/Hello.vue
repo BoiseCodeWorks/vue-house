6ffb7c4344ed6e95ce6006abe090a58cbdbea64b
Jake Overall <joverall22@gmail.com>
2017-02-13 11:54:10 -0700

    pushy students

---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="house">
+      <room v-for="r in house.rooms" :room="r"></room>
+  </div>
+</template>
+
+<script>
+import Room from './Room'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      house: {
+        rooms: [{
+          name: 'Living Room',
+          dimensions:{
+            height: 10,
+            width: 20
+          }
+        },{
+          name: 'Master',
+          dimensions:{
+            height: 10,
+            width: 10
+          }
+        }]
+      }
+    }
+  },
+  components: {
+    Room
+  }
+}
+</script>
+
+
+<style lang="stylus">
+.house
+  width: 100%
+  min-height: 800px
+  border: 1px solid black
+
+.room
+  width: 200px
+  height: 200px
+  border: 1px dashed black
+  display: inline-block
+
+</style>
---
+++ src/components/Room.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="room" :style="{
+    height: room.dimensions.height * 10 + 'px', 
+    width: room.dimensions.width * 10 + 'px'
+    }">
+    <input type="text" v-model="room.dimensions.height">
+    <span>X</span>
+    <input type="text" v-model="room.dimensions.width">
+    <h2>{{room.name}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'room',
+  props: ['room'],
+  data(){
+    return {
+      test: 'Hello this is the room'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
